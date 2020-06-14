ec8e528178c731320548248d8074bf33f98caeba
Anton Omelchuk <kino_kg@ukr.net>
2020-06-14 20:58:17 +0300

    add full movement of piece and check position of piece inside playfield

--- src/game.js
+++ src/game.js
@@ -16,22 +16,61 @@ export default class Game {
   move(direction) {
     switch(direction) {
       case 'ArrowRight':
-        this.activePiece.x + 1 !== 10 && (this.activePiece.x += 1)
+        this.activePiece.x + 1 !== 10 && this.isPieceOutBoundaries(9) && (this.activePiece.x += 1)
+        this.lockPiece()
         break
       case 'ArrowLeft':
-        this.activePiece.x - 1 !== -1 && (this.activePiece.x -= 1)
+        this.activePiece.x - 1 !== -1 && this.isPieceOutBoundaries(0) && (this.activePiece.x -= 1)
+        this.lockPiece()
         break
       case 'ArrowDown':
-        this.activePiece.y + 1 !== 20 && (this.activePiece.y += 1)
+        this.activePiece.y + 1 !== 20 && this.isPieceOutBoundaries(undefined, 19) && (this.activePiece.y += 1)
+        this.lockPiece()
         break
+    }
+  }
+
+  isPieceOutBoundaries(pieceX = undefined, pieceY = undefined) {
+    const playfield = this.playfield
+
+    if(pieceX) {
+      for(let y = 0; y < playfield.length; y++) {
+        for(let x = 0; x < playfield[y].length; x++) {
+          if(playfield[y][pieceX] === 1) {
+            return false
+          }
+        }
+      }
+    }
+    if(pieceY) {
+      for(let y = 0; y < playfield.length; y++) {
+        for(let x = 0; x < playfield[y].length; x++) {
+          if(playfield[pieceY][x] === 1) {
+            return false
+          }
+        }
+      }
+    }
+    return true
+  }
+
+  clearPlayfield() {
+    const playfield = this.playfield
+    for(let y = 0; y < playfield.length; y++) {
+      for(let x = 0; x < playfield[y].length; x++) {
+        playfield[y][x] = 0
+      }
     }
   }
 
   lockPiece() {
     const { x: pieceX, y: pieceY, blocks } = this.activePiece
 
+    this.clearPlayfield()
+
     for(let y = 0; y < blocks.length; y++) {
       for(let x = 0; x < blocks[y].length; x++) {
+        if(blocks[y][x])
         this.playfield[pieceY + y][pieceX + x] = blocks[y][x]
       }
     }
