317f6baa2a8a8815767fd54cfe683f6ef35f23a7
Anton Omelchuk <kino_kg@ukr.net>
2020-06-15 21:22:51 +0300

    code refactoring

--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@ export default class Game {
   score = 0
   lines = 0
   level = 0
-  playfield = new Array(20).fill(0).map(col => col = new Array(10).fill(0))
+  playfield = new Array(20).fill().map(col => col = new Array(10).fill(0))
   activePiece = {
     x: 0,
     y: 0,
@@ -16,62 +16,62 @@ export default class Game {
   move(direction) {
     switch(direction) {
       case 'ArrowRight':
-        this.activePiece.x + 1 !== 10 && this.isPieceOutBoundaries(9) && (this.activePiece.x += 1)
-        this.lockPiece()
+        this.activePiece.x += 1
+
+        if(this.checkPiecePosition()) {
+          this.activePiece.x -= 1
+          return this.lockPiece()
+        } 
+
         break
       case 'ArrowLeft':
-        this.activePiece.x - 1 !== -1 && this.isPieceOutBoundaries(0) && (this.activePiece.x -= 1)
-        this.lockPiece()
+        this.activePiece.x -= 1
+
+        if(this.checkPiecePosition()) {
+          this.activePiece.x += 1
+          return this.lockPiece()
+        } 
+
         break
       case 'ArrowDown':
-        this.activePiece.y + 1 !== 20 && this.isPieceOutBoundaries(undefined, 19) && (this.activePiece.y += 1)
-        this.lockPiece()
+        this.activePiece.y += 1
+
+        if(this.checkPiecePosition()) {
+          this.activePiece.y -= 1
+          return this.lockPiece()
+        } 
+
         break
     }
   }
 
-  isPieceOutBoundaries(pieceX = undefined, pieceY = undefined) {
+  checkPiecePosition() {
     const playfield = this.playfield
-
-    if(pieceX) {
-      for(let y = 0; y < playfield.length; y++) {
-        for(let x = 0; x < playfield[y].length; x++) {
-          if(playfield[y][pieceX] === 1) {
-            return false
-          }
-        }
-      }
-    }
-    if(pieceY) {
-      for(let y = 0; y < playfield.length; y++) {
-        for(let x = 0; x < playfield[y].length; x++) {
-          if(playfield[pieceY][x] === 1) {
-            return false
-          }
-        }
-      }
-    }
-    return true
-  }
-
-  clearPlayfield() {
-    const playfield = this.playfield
-    for(let y = 0; y < playfield.length; y++) {
-      for(let x = 0; x < playfield[y].length; x++) {
-        playfield[y][x] = 0
-      }
-    }
-  }
-
-  lockPiece() {
-    const { x: pieceX, y: pieceY, blocks } = this.activePiece
-
-    this.clearPlayfield()
+    const { y: pieceY, x: pieceX, blocks } = this.activePiece
 
     for(let y = 0; y < blocks.length; y++) {
       for(let x = 0; x < blocks[y].length; x++) {
-        if(blocks[y][x])
-        this.playfield[pieceY + y][pieceX + x] = blocks[y][x]
+        if(
+            blocks[y][x] && 
+            ((playfield[pieceY + y] === undefined || playfield[pieceY][pieceX + x] === undefined) ||
+            playfield[pieceY + y][pieceX + x])
+          ) {
+            return true
+          }
+          
+      }
+    }
+
+    return false
+  }
+
+  lockPiece() {
+    const { y: pieceY, x: pieceX, blocks } = this.activePiece
+    for(let y = 0; y < blocks.length; y++) {
+      for(let x = 0; x < blocks[y].length; x++) {
+        if(blocks[y][x]) {
+          this.playfield[pieceY + y][pieceX + x] = blocks[y][x]
+        }
       }
     }
   }
