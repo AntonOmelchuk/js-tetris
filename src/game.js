990e40d23eb964a05b1ad0214c036705b081a94a
Anton Omelchuk <kino_kg@ukr.net>
2020-07-01 21:11:38 +0300

    refactoring code

--- src/game.js
+++ src/game.js
@@ -2,35 +2,36 @@ export default class Game {
   score = 0
   lines = 0
   level = 0
-  playfield = new Array(20).fill().map(col => col = new Array(10).fill(0))
+  // playfield = new Array(20).fill().map(col => col = new Array(10).fill(0))
+  playfield = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]
   activePiece = {
     x: 0,
     y: 0,
-    get blocks() {
-      return this.rotations[this.rotationIndex]
-    },
-    rotationIndex: 0,
-    rotations: [
-      [
-        [0, 1, 0],
-        [1, 1, 1],
-        [0, 0, 0],
-      ],
-      [
-        [0, 1, 0],
-        [0, 1, 1],
-        [0, 1, 0],
-      ],
-      [
-        [0, 0, 0],
-        [1, 1, 1],
-        [0, 1, 0],
-      ],
-      [
-        [0, 1, 0],
-        [1, 1, 0],
-        [0, 1, 0],
-      ],
+    blocks: [
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0]
     ]
   }
 
@@ -48,63 +49,75 @@ export default class Game {
     return playfield
   }
 
-  move(direction) {
-    switch(direction) {
-      case 'ArrowRight':
-        console.log('move right')
-        this.activePiece.x += 1
+  movePieceLeft() {
+    this.activePiece.x -= 1
 
-        if(this.checkPiecePosition()) {
-          this.activePiece.x -= 1
-          return this.lockPiece()
-        }
+    if(this.hasCollision()) {
+      this.activePiece.x += 1
+    }
+  }
 
-        break
-      case 'ArrowLeft':
-        this.activePiece.x -= 1
+  movePieceRight() {
+    this.activePiece.x += 1
 
-        if(this.checkPiecePosition()) {
-          this.activePiece.x += 1
-          return this.lockPiece()
-        }
+    if(this.hasCollision()) {
+      this.activePiece.x -= 1
+    }
+  }
 
-        break
-      case 'ArrowDown':
-        this.activePiece.y += 1
+  movePieceDown() {
+    this.activePiece.y += 1
 
-        if(this.checkPiecePosition()) {
-          this.activePiece.y -= 1
-          return this.lockPiece()
-        }
-
-        break
+    if(this.hasCollision()) {
+      this.activePiece.y -= 1
+      this.lockPiece()
     }
   }
 
   rotatePiece() {
-    this.activePiece.rotationIndex = this.activePiece.rotationIndex < 3 ? this.activePiece.rotationIndex + 1 : 0
+    this.rotateBlocks()
 
-    if(this.checkPiecePosition()) {
-      this.activePiece.rotationIndex = this.activePiece.rotationIndex > 0 ? this.activePiece.rotationIndex - 1 : 3
+    if(this.hasCollision()) {
+      this.rotateBlocks(false)
     }
-
-    return this.activePiece.blocks
   }
 
-  checkPiecePosition() {
-    const playfield = this.playfield
+  rotateBlocks(clockwise = true) {
+    const blocks = this.activePiece.blocks
+    const length = blocks.length
+    const x = Math.floor(length / 2)
+    const y = length - 1
+
+    for (let i = 0; i < x; i++) {
+      for (let j = i; j < y - i; j++) {
+        const temp = blocks[i][j]
+
+        if(clockwise) {
+          blocks[i][j] = blocks[y - j][i]
+          blocks[y - j][i] = blocks[y - i][y - j]
+          blocks[y - i][y - j] = blocks[j][y - i]
+          blocks[j][y - i] = temp
+        } else {
+          blocks[i][j] = blocks[j][y - i]
+          blocks[j][y - i] = blocks[y - i][y - j]
+          blocks[y - i][y - j] = blocks[y - j][i]
+          blocks[y - j][i] = temp
+        }
+      }
+    }
+  }
+
+  hasCollision() {
     const { y: pieceY, x: pieceX, blocks } = this.activePiece
 
     for(let y = 0; y < blocks.length; y++) {
       for(let x = 0; x < blocks[y].length; x++) {
-        if(
-            blocks[y][x] &&
-            ((playfield[pieceY + y] === undefined || playfield[pieceY][pieceX + x] === undefined) ||
-            playfield[pieceY + y][pieceX + x])
+        if(blocks[y][x]
+          && ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined)
+          || this.playfield[pieceY + y][pieceX + x])
           ) {
-            return true
-          }
-
+          return true
+        }
       }
     }
 
@@ -113,6 +126,7 @@ export default class Game {
 
   lockPiece() {
     const { y: pieceY, x: pieceX, blocks } = this.activePiece
+
     for(let y = 0; y < blocks.length; y++) {
       for(let x = 0; x < blocks[y].length; x++) {
         if(blocks[y][x]) {
