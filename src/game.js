042b9b2b53b1d5cc01dce91caacf96d931bbdd16
Anton Omelchuk <kino_kg@ukr.net>
2020-07-01 20:04:51 +0300

    add rotation piece

--- src/game.js
+++ src/game.js
@@ -6,16 +6,52 @@ export default class Game {
   activePiece = {
     x: 0,
     y: 0,
-    blocks: [
-      [0, 1, 0],
-      [1, 1, 1],
-      [0, 0, 0],
+    get blocks() {
+      return this.rotations[this.rotationIndex]
+    },
+    rotationIndex: 0,
+    rotations: [
+      [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 0, 0],
+      ],
+      [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 1, 0],
+      ],
+      [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 1, 0],
+      ],
+      [
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 1, 0],
+      ],
     ]
+  }
+
+  getState() {
+    const playfield = [...this.playfield]
+
+    for(let y = 0; y < this.activePiece.blocks.length; y++) {
+      for(let x = 0; x < this.activePiece.blocks[y].length; x++) {
+        if(this.activePiece.blocks[y][x]) {
+          playfield[this.activePiece.y + y][this.activePiece.x + x] = this.activePiece.blocks[y][x]
+        }
+      }
+    }
+
+    return playfield
   }
 
   move(direction) {
     switch(direction) {
       case 'ArrowRight':
+        console.log('move right')
         this.activePiece.x += 1
 
         if(this.checkPiecePosition()) {
@@ -43,6 +79,16 @@ export default class Game {
 
         break
     }
+  }
+
+  rotatePiece() {
+    this.activePiece.rotationIndex = this.activePiece.rotationIndex < 3 ? this.activePiece.rotationIndex + 1 : 0
+
+    if(this.checkPiecePosition()) {
+      this.activePiece.rotationIndex = this.activePiece.rotationIndex > 0 ? this.activePiece.rotationIndex - 1 : 3
+    }
+
+    return this.activePiece.blocks
   }
 
   checkPiecePosition() {
