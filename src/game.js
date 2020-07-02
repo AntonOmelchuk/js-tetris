e00d3591c1823c186f48d31984d45a71ee5ba152
Anton Omelchuk <kino_kg@ukr.net>
2020-07-02 20:59:48 +0300

    added pieces

--- src/game.js
+++ src/game.js
@@ -3,15 +3,8 @@ export default class Game {
   lines = 0
   level = 0
   playfield = new Array(20).fill().map(col => col = new Array(10).fill(0))
-  activePiece = {
-    x: 0,
-    y: 0,
-    blocks: [
-      [0, 1, 0],
-      [1, 1, 1],
-      [0, 0, 0]
-    ]
-  }
+  activePiece = this.createPiece()
+  nextPiece = this.createPiece()
 
   getState() {
     const playfield = this.createPlayfield()
@@ -50,6 +43,70 @@ export default class Game {
     return playfield
   }
 
+  createPiece() {
+    const index = Math.floor(Math.random() * 7)
+    const type = 'IJLOSTZ'[index]
+    const piece = { x: 0, y: 0 }
+
+    switch(type) {
+      case 'I':
+        piece.blocks = [
+          [0, 0, 0, 0],
+          [1, 1, 1, 1],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0]
+        ]
+        break
+      case 'J':
+        piece.blocks = [
+          [0, 0, 0],
+          [2, 2, 2],
+          [0, 0, 2]
+        ]
+        break
+      case 'L':
+        piece.blocks = [
+          [0, 0, 0],
+          [3, 3, 3],
+          [3, 0, 0],
+        ]
+        break
+      case 'O':
+        piece.blocks = [
+          [0, 0, 0, 0],
+          [0, 4, 4, 0],
+          [0, 4, 4, 0],
+          [0, 0, 0, 0]
+        ]
+        break
+      case 'S':
+        piece.blocks = [
+          [0, 0, 0],
+          [0, 5, 5],
+          [5, 5, 0],
+        ]
+        break
+      case 'T':
+        piece.blocks = [
+          [0, 0, 0],
+          [6, 6, 6],
+          [0, 6, 0],
+        ]
+        break
+      case 'Z':
+        piece.blocks = [
+          [0, 0, 0],
+          [7, 7, 0],
+          [0, 7, 7],
+        ]
+        break
+      default:
+        throw new Error('Unknown type of piece')
+    }
+
+    return piece
+  }
+
   movePieceLeft() {
     this.activePiece.x -= 1
 
@@ -72,6 +129,7 @@ export default class Game {
     if(this.hasCollision()) {
       this.activePiece.y -= 1
       this.lockPiece()
+      this.updatePieces()
     }
   }
 
@@ -135,5 +193,10 @@ export default class Game {
         }
       }
     }
+  }
+
+  updatePieces() {
+    this.activePiece = this.nextPiece
+    this.nextPiece = this.createPiece()
   }
 }
